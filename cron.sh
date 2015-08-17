4eb99d4ebb9551bf5abab2f5bcd077306074e0a2
Nick Freear <nfreear@yahoo.co.uk>
2015-08-17 16:41:16 +0100

    Initial commit for iet-satis [iet:3900233]
    * Gist: https://gist.github.com/nfreear/f7c2e1d5ee102932cc27

---
+++ cron.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Re-build Satis repository via a Crontab.
+#
+# Nick Freear, 17 July 2015.
+#
+# Crontab:
+# min hour dom mo dow  command
+# 1 10,13,16,19,22 * * *  bash ... /dev_projects/satis-repo/iet-satis.sh
+
+
+export HTTP_PROXY=wwwcache.open.ac.uk:80
+export HTTPS_PROXY=wwwcache.open.ac.uk:80
+
+
+date +">> Start Satis re-build:  %Y-%m-%d %T"
+
+cd ... /dev_projects/satis-repo/satis
+
+php bin/satis build ../satis.json ../web >> $HOME/crontab-satis.log 2>&1
+
+#End.
